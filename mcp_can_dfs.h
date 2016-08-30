b62c1331dc7e1f7b087c3eef10f0529c1f5f9837
Muxa1L <mimih1990@gmail.com>
2016-08-30 14:50:49 +0300

    Update mcp_can_dfs.h

--- mcp_can_dfs.h
+++ mcp_can_dfs.h
@@ -447,16 +447,17 @@
 #define CAN_10KBPS   2
 #define CAN_20KBPS   3
 #define CAN_31K25BPS 4
-#define CAN_40KBPS   5
-#define CAN_50KBPS   6
-#define CAN_80KBPS   7
-#define CAN_95KBPS   8
-#define CAN_100KBPS  9
-#define CAN_125KBPS  10
-#define CAN_200KBPS  11
-#define CAN_250KBPS  12
-#define CAN_500KBPS  13
-#define CAN_1000KBPS 14
+#define CAN_33K33BPS 5
+#define CAN_40KBPS   6
+#define CAN_50KBPS   7
+#define CAN_80KBPS   8
+#define CAN_95KBPS   9
+#define CAN_100KBPS  10
+#define CAN_125KBPS  11
+#define CAN_200KBPS  12
+#define CAN_250KBPS  13
+#define CAN_500KBPS  14
+#define CAN_1000KBPS 15
 
 #define CAN_OK             (0)
 #define CAN_FAILINIT       (1)
